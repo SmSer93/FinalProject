30878e8f01b30042da6c991d5253f0fbd8b48af5
Sergey <segarik93@gmail.com>
2022-12-26 22:57:37 +0300

    Programm end.

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,7 +1,79 @@
-﻿// Принцип работы программы
-// У пользователя спрашивается количество строк, которые он введёт
-// Создается массив на это количество ячеек
-// В цикле пользователь вводит следующую строку
-// По окончании цикла создается выходной массив, равный по длине исходному, и ещё один счётчик
-// Запускается цикл на проверку длины строки(если длина меньше 4, то переносится в новый массив, если нет - пропускается)
-// С помощью функции новый массив выводится на экран(пустые элементы не печатаются)
+﻿//Написать программу, которая из имеющегося массива строк сформирует новый массив.
+//Условие нового массива - длина строки не больше 3 символов.
+
+void PrintText(string text)
+{
+    Console.Write(text);
+}
+
+void NewLine ()
+{
+    Console.WriteLine();
+}
+
+dynamic UserEnter()
+{
+    string a = Console.ReadLine();
+    if (a != "")
+    {
+        if (int.TryParse(a, out int n))
+        {
+            int b = Convert.ToInt32(a);
+            return b;
+        }
+        else if (a == "y") return true;
+        else if (a == "n") return false;
+        else return a;
+    }
+    else
+    {
+        a = "exit";
+        return a;
+    }
+}
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] != null)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.WriteLine();
+}
+
+string[] UserEnterStrings()
+{
+    PrintText("Введите до 30 строк. Если хотите завершить ввод - нажмите 'Enter'.");
+    NewLine();
+    string[] help = new string[30];
+    int i = 0;
+    for (; true; i++)
+{
+    string str = String.Empty;
+    if (i < 30)
+    {
+        PrintText($"Введите {i + 1} строку: ");
+        str = Convert.ToString(UserEnter());
+    }
+    if (str == "exit" || i == 30) break;
+    else help[i] = str;
+}
+string[] strings = new string [i];
+for (int j=0; j < strings.Length; j++)
+{
+    strings[j] = help[j];
+}
+return strings;
+}
+
+string[] enter = UserEnterStrings();
+string[] exit = new string[enter.Length];
+for (int i = 0; i < enter.Length; i++)
+{
+    if (enter[i].Length < 4) exit[i] = enter[i];
+}
+
+PrintArray(exit);
